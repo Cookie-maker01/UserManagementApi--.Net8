d8fd55be9ff0d79866cdadf5f9e9768190919dfd
unknown <weifang060@gmail.com>
2026-04-07 13:14:31 +1200

    Creat User file

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+namespace UserManagementApi.Models;
+
+public class User
+{
+  public int Id { get; set; }
+
+  public string Username { get; set; } = "";
+
+  public string Email { get; set; } = "";
+
+  public string PasswordHash { get; set; } = "";
+
+  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+
--- obj/Debug/net8.0/UserManagementApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bee88e7215d214e711e50e682b80876264a0d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff03c6e9b2f38bb1278fc2644d0c03189eeee9d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
