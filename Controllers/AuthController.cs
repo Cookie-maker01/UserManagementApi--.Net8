579786e307f0b8b3c7eb3f20b96bcc8b9251cde5
unknown <weifang060@gmail.com>
2026-04-08 15:52:20 +1200

    Modify Login approch

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,6 +4,10 @@ using UserManagementApi.DTOs;
 using UserManagementApi.Models;
 using BCrypt.Net;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace UserManagementApi.Controllers;
 
@@ -43,6 +47,24 @@ public class AuthController : ControllerBase
     if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
       return BadRequest("Invalid password");
 
-      return Ok("Login successful");
+    var claims = new[]
+    {
+      new Claim(ClaimTypes.Name, user.Username),
+      new Claim(ClaimTypes.Email, user.Email)
+    };
+
+    var key = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes("THIS_IS_MY_SECRET_KEY_654321"));
+
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    var token = new JwtSecurityToken(
+        claims: claims,
+        expires: DateTime.Now.AddHours(1),
+        signingCredentials: creds);
+    
+    var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+    return Ok (new { token = jwt });
   }
 }
