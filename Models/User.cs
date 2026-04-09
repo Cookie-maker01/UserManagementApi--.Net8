5cc76d6edbed5ee1cadc5fcae5ab163b04c4ab27
unknown <weifang060@gmail.com>
2026-04-09 16:54:43 +1200

    Add Role in Model User

--- Models/User.cs
+++ Models/User.cs
@@ -10,6 +10,8 @@ public class User
 
   public string PasswordHash { get; set; } = "";
 
+  public string Role { get; set; } = "User";
+
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
 
