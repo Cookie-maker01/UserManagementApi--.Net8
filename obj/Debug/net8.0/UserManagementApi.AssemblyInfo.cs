ea60822e3c9ea79265758a4c1ab41e96cdbd918c
unknown <weifang060@gmail.com>
2026-04-10 18:47:07 +1200

    Modify AppDbContext file

--- obj/Debug/net8.0/UserManagementApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b055fe290e8faec14a18f477e2c0a843b5c621bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972c1374574354ccbd7baa42ea9a1243d7e67b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
